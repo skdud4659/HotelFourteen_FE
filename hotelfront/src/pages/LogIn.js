456610b56718ef66fb9f8150d8c7d076be1f4c07
skdud4659 <skdud4659@gmail.com>
2021-07-09 14:53:08 +0900

    프론트단 기본 틀 update

---
+++ hotelfront/src/pages/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = (props) => {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
+
+export default LogIn;
---
+++ hotelfront/src/pages/WriteEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WriteEdit = (props) => {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
+
+export default WriteEdit;
