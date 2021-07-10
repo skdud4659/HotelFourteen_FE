7ec3862a1c7d2efb252dd2eba79ee860e820a902
wglee0511 <ranazang@naver.com>
2021-07-10 21:19:55 +0900

    Connect each page

--- hotelfront/src/index.js
+++ hotelfront/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './shared/App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./shared/App";
+import reportWebVitals from "./reportWebVitals";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "./redux/configStore";
 import store from "./redux/configStore";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- hotelfront/src/pages/LogIn.js
+++ hotelfront/src/pages/LogIn.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Button, Grid, Input, Text } from '../elements';
+import React from "react";
+import styled from "styled-components";
+import { Button, Grid, Input, Text } from "../elements";
 
 const LogIn = (props) => {
   //input 입력값 확인을 위한 함수
@@ -17,7 +17,6 @@ const LogIn = (props) => {
   // //로그인 버튼
   // const loginBtn = () => {
   //   console.log(id, pw)
-
 
   //input 입력값 확인을 위한 함수
   // const [user_name, setUserName] = React.useState("");
@@ -37,59 +36,59 @@ const LogIn = (props) => {
   // }
 
   return (
-         //로그인 레이아웃 
-        //inputboxs
-        <Grid bg={'#1f2e3b'} width="60%" height="100%" margin="7% auto">
-          {/* register글씨  */}
-          <Box>
-            <Text color="white" size="60px" bold>Login</Text>
-          </Box>
-          {/* //action="" accept-charset="utf-8" method="post"  */}
-          <FromBox name="registerForm">
-            <fieldset>
-              {/* 닉네임 input */}
-              <Grid width="400px" height="50px" padding="5% 0px">
-                <Text bold color="white">아이디</Text>
-                <Input
-                  padding="0px 1%;"
-                  placeholder="아이디를 입력해주세요."
-                  // _onChange={input_Id}
-                  name="id"
-                <Text bold color="white">닉네임</Text>
-                <Input
-                  padding="0px 1%;"
-                  placeholder="닉네임을 입력해주세요."
-                  // _onChange={input_UserName}
-                  name="username"
-                  />
-              </Grid>
-              {/* 비밀번호 input */}
-              <Grid width="400px" height="50px" padding="5% 0px">
-                <Text bold color="white">비밀번호486</Text>
-                <Input
-                  padding="0px 1%;"
-                  placeholder="비밀번호를 입력해주세요."
-                  // _onChange={input_Pw}
-                  name="pwd"
-                  />
-              </Grid>
-              {/* 로그인 버튼 */}
-              <Grid width="150px" height="70px" padding="10% 0px 7% 0px">
-              <InputBtn type="submit" value="로그인"/>
-              {/* <Button
+    //로그인 레이아웃
+    //inputboxs
+    <Grid bg={"#1f2e3b"} width="60%" height="100%" margin="7% auto">
+      {/* register글씨  */}
+      <Box>
+        <Text color="white" size="60px" bold>
+          Login
+        </Text>
+      </Box>
+      {/* //action="" accept-charset="utf-8" method="post"  */}
+      <FromBox name="registerForm">
+        <fieldset>
+          {/* 닉네임 input */}
+          <Grid width="400px" height="50px" padding="5% 0px">
+            <Text bold color="white">
+              아이디
+            </Text>
+            <Input
+              padding="0px 1%;"
+              placeholder="아이디를 입력해주세요."
+              // _onChange={input_Id}
+              name="id"
+            />
+          </Grid>
+          {/* 비밀번호 input */}
+          <Grid width="400px" height="50px" padding="5% 0px">
+            <Text bold color="white">
+              비밀번호486
+            </Text>
+            <Input
+              padding="0px 1%;"
+              placeholder="비밀번호를 입력해주세요."
+              // _onChange={input_Pw}
+              name="pwd"
+            />
+          </Grid>
+          {/* 로그인 버튼 */}
+          <Grid width="150px" height="70px" padding="10% 0px 7% 0px">
+            <InputBtn type="submit" value="로그인" />
+            {/* <Button
                 bg_color={"#376a48"}
                 _onClick={loginBtn}>로그인</Button> */}
-              </Grid>
-          </fieldset>
-        </FromBox>
-      </Grid>
+          </Grid>
+        </fieldset>
+      </FromBox>
+    </Grid>
   );
-}
+};
 
 const Box = styled.div`
-  width:100%;
-  height:10%;
-  padding:auto;
+  width: 100%;
+  height: 10%;
+  padding: auto;
   text-align: center;
   padding-top: 5%;
 `;
@@ -97,18 +96,18 @@ const Box = styled.div`
 const FromBox = styled.form`
   width: 70%;
   height: 90%;
-  margin:5% auto;
+  margin: 5% auto;
 `;
 
 const InputBtn = styled.input`
-  width:100%;
+  width: 100%;
   height: 40px;
   border-radius: 30px;
   font-weight: 700;
   border: none;
-  color:white;
+  color: white;
   background-color: #376a48;
-  margin:auto;
+  margin: auto;
   align-content: center;
 `;
 
