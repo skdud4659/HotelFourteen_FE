0a8a4ea987eaa530e72f4d0ca0519c669f77d385
skdud4659 <skdud4659@gmail.com>
2021-07-13 16:23:05 +0900

    마이페이지

--- hotelfront/src/pages/Mypage.js
+++ hotelfront/src/pages/Mypage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Button, Input, Text } from "../elements";
 import theme from "../shared/theme";
+import Book_Detail from '../components/Book_Detail';
 
 const MyPage = (props) => {
   return (
@@ -8,12 +9,12 @@ const MyPage = (props) => {
       {/* 레이아웃 */}
       <Grid
         bg={theme.gridBgColor}
-        width="70%"
+        width="800px"
         height="100vh"
         margin="2% auto"
-        padding="5% 3% 0px 6%"
+        padding="5% 3%"
         border_radius={theme.borderRadius}>
-          
+          <Book_Detail/>
       </Grid>
     </React.Fragment>
   );
